8eb0a9a09f31a3a40230837b864aa9a3c9d0352b
KazuhitoW <kazushi16@gmail.com>
2019-11-30 12:49:02 +0900

    first_commit

---
+++ script1.js
@@ -0,0 +1,31 @@
+$(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('nav').css('background-color', '#f8f9fa');
+            // $('nav').animate('background-color', '#6c757d');
+        } else {
+            $('nav').css('background', 'none');
+        }
+    });
+    $('.faq-list-item').click(function(){
+        var $answer = $(this).find('.answer');
+        if ($answer.hasClass('open')) {
+            $answer.removeClass('open');
+            $answer.slideUp();
+            $(this).find('span').text('+');
+        } else {
+            $answer.addClass('open');
+            $answer.slideDown();
+            $(this).find('span').text('-');
+        }
+    });
+    $('a[href^="#"]').click(function() {
+        // スクロールの速度
+        var speed = 1000; // ミリ秒で記述
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+    });
+});
