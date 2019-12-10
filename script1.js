4300ce3610f992bd16bde77b72adfef104eec647
KazuhitoW <kazushi16@gmail.com>
2019-12-10 22:19:40 +0900

    mouse stalker add and tag name change
    マウスストーカーの追加とhtmlタグ名の変更

--- script1.js
+++ script1.js
@@ -28,4 +28,12 @@ $(function(){
         $('body,html').animate({scrollTop:position}, speed, 'swing');
         return false;
     });
+
+     //マウスストーカー用のdivを取得
+    const stalker = document.getElementById('stalker'); 
+
+    //上記のdivタグをマウスに追従させる処理
+    document.addEventListener('mousemove', function (e) {
+        stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+    });
 });
